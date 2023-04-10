a9f6cd72067881296d8771c713e5d7e8bcc4b062
jeongwoo An <ajw2312@gmail.com>
2023-04-10 11:30:41 +0900

    피보나치 알고리즘 dp

--- javascript/fibo.js
+++ javascript/fibo.js
@@ -1,10 +1,20 @@
-// 피보나치 함수를 단순 재귀 코드로 작성
-function fibo(x) {
-    if (x === 1 || x === 2) {
-      return 1;
-    }
-    // 재귀적으로 호출하며 피보나치 수열의 항을 구함
-    return fibo(x - 1) + fibo(x - 2);
-}
+function fibonacci (n, pastResults)
+{
+  var memo = pastResults || [1, 1]; //if no pastResults, set default value
+  var i = memo.length;
+  if (i > n) { // we have result 
+    console.log('looking up value')
+    return {  //return 2 values
+        pastResults: memo,
+        result: memo[n-1]
+    };  // return it
+  }
+  for (; i < n; i++) {
+    memo[i] = memo[i-1] + memo[i-2];
+  }
 
-console.log(fibo(10));
+  return {
+    pastResults: memo,
+    result: memo[n-1]
+  };
+}
